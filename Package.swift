e06e5f22b41074d5039c61c8ae7a3b98bb642ba9
Rudrank Riyam <rudrankriyam@gmail.com>
2025-06-22 17:20:40 -0700

    Update Package.swift to use Swift tools version 6.0

--- Package.swift
+++ Package.swift
@@ -1,32 +1,26 @@
-// swift-tools-version: 6.2
+// swift-tools-version: 6.0
 import PackageDescription
 
 let package = Package(
-    name: "AuralKit",
-    platforms: [
-        .iOS(.v17),
-        .macOS(.v14),
-        .visionOS(.v1)
-    ],
-    products: [
-        .library(
-            name: "AuralKit",
-            targets: ["AuralKit"]
-        )
-    ],
-    targets: [
-        .target(
-            name: "AuralKit",
-            swiftSettings: [
-                .enableUpcomingFeature("StrictConcurrency")
-            ]
-        ),
-        .testTarget(
-            name: "AuralKitTests",
-            dependencies: ["AuralKit"],
-            swiftSettings: [
-                .enableUpcomingFeature("StrictConcurrency")
-            ]
-        )
-    ]
+  name: "AuralKit",
+  platforms: [
+    .iOS(.v17),
+    .macOS(.v14),
+    .visionOS(.v1),
+  ],
+  products: [
+    .library(
+      name: "AuralKit",
+      targets: ["AuralKit"]
+    )
+  ],
+  targets: [
+    .target(
+      name: "AuralKit"
+    ),
+    .testTarget(
+      name: "AuralKitTests",
+      dependencies: ["AuralKit"]
+    ),
+  ]
 )
